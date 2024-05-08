a1e934eac49583a27c3a05eca22a3b4a39c0b2a9
Alain Martin <hexastix@gmail.com>
2024-05-08 19:09:30 +0200

    Make script download files as well

--- sync.py
+++ sync.py
@@ -1,20 +1,48 @@
 import os
 import sys
+import urllib.parse
 import urllib.request
 
 from html.parser import HTMLParser
 
 
+URL_PREFIXES = (
+    "https://cdn.ravensburger.com/lorcana/",
+    "https://files.disneylorcana.com/",
+)
+
+
 class ResourcesHTMLParser(HTMLParser):
-    def __init__(self, readme_file):
+    def __init__(self, readme_file, output_dir):
         super().__init__()
 
         self.readme_file = readme_file
+        self.output_dir = output_dir
 
         self.in_faq_section = False
         self.in_faq_block_title = False
         self.in_accordion_header = False
         self.href = None
+
+    def download_file(self, url):
+        request = urllib.request.Request(url)
+        request.add_header("User-Agent", "")
+
+        result = urllib.parse.urlparse(url)
+        relative_url = f"{result.netloc.replace('.', '_')}{result.path}"
+        if not relative_url.endswith(".pdf"):
+            relative_url = f"{relative_url}.pdf"
+
+        file_path = os.path.join(self.output_dir, urllib.parse.unquote(relative_url))
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        print(f"Downloading {url} to {file_path}")
+
+        with urllib.request.urlopen(request) as r:
+            with open(file_path, "wb") as w:
+                w.write(r.read())
+
+        return relative_url
 
     def handle_starttag(self, tag, attrs):
         if self.in_faq_section:
@@ -34,7 +62,10 @@ class ResourcesHTMLParser(HTMLParser):
                 for attr_name, attr_value in attrs:
                     if attr_name == "href":
                         assert self.href is None
-                        self.href = attr_value
+                        if attr_value.startswith(URL_PREFIXES):
+                            self.href = self.download_file(attr_value)
+                        else:
+                            self.href = attr_value
                         break
 
         elif tag == "section":
@@ -77,7 +108,7 @@ def main():
     with open(os.path.join(output_dir, "README.md"), "w") as readme_file:
         print("# Disney Lorcana TCG Resources", file=readme_file)
 
-        parser = ResourcesHTMLParser(readme_file)
+        parser = ResourcesHTMLParser(readme_file, output_dir)
         parser.feed(contents)
 
 
